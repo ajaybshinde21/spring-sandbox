3dd689464d1ba2172c5692122d7ab825b8d92a73
ajaybshinde21 <ajayshinde1@protonmail.com>
2022-01-28 20:45:39 +0530

    Implement createTable method

--- my-app/src/main/java/databaseClient/JdbcClient.java
+++ my-app/src/main/java/databaseClient/JdbcClient.java
@@ -1,15 +1,32 @@
 package databaseClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import entities.User;
+import entities.Ui;
 @Component
 public class JdbcClient {
-  JdbcTemplate jdbcTemplate;
+  private JdbcTemplate jdbcTemplate;
+  private Ui ui;
   @Autowired
- public  JdbcClient(JdbcTemplate jdbcTemplate){
+ public  JdbcClient(JdbcTemplate jdbcTemplate, Ui ui){
     this.jdbcTemplate = jdbcTemplate;
+    this.ui = ui;
+    createTable("User");
  }
+public void insertUser(User u) {
 
+}
+private void createTable(String tableName) {
+ try{
+   jdbcTemplate.update("CREATE TABLE IF NOT EXISTS User (id INTEGER, name VARCHAR(20), username VARCHAR(20), password VARCHAR(20))");   
+ }catch(DataAccessException dae){
+    ui.showError(dae.getMessage());
+    ui.printStackTrace(dae.getStackTrace());
+ }
+   
+}
 }
