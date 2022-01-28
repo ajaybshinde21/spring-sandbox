b39ea960b18db8f6da17a0b0985886d44c9a6510
ajaybshinde21 <ajayshinde1@protonmail.com>
2022-01-28 20:45:00 +0530

    Create printError method

--- my-app/src/main/java/entities/Ui.java
+++ my-app/src/main/java/entities/Ui.java
@@ -7,4 +7,7 @@ public class Ui {
   public void showError(String s){
     System.out.println(s);
   }
+  public void printStackTrace(StackTraceElement[] stackTraceElements){
+    // to be implemented
+  }
 }
